7470461b33dd1d6e5a65bb619e679347404d80e1
niceoneallround <niceoneallround@gmail.com>
2016-02-25 11:01:05 -0800

    Change findObjects to return null if cannot find and add more tests

--- test/testJsonldUtilsTransforms.js
+++ test/testJsonldUtilsTransforms.js
@@ -3,9 +3,10 @@
 //
 // Test JSONLD utils
 //
-var //assert = require('assert'),
+var assert = require('assert'),
     should = require('should'),
-    jsonldUtils = require('../lib/jldUtils');
+    jsonldUtils = require('../lib/jldUtils'),
+    util = require('util');
 
 describe('jsonld utils fetch objects', function () {
   'use strict';
@@ -43,7 +44,7 @@ describe('jsonld utils fetch objects', function () {
     ]
   };
 
-  console.log('Base doc:%j', doc);
+  //console.log('Base doc:%j', doc);
 
   describe('1 test finding objects of type in graph', function () {
 
@@ -52,7 +53,7 @@ describe('jsonld utils fetch objects', function () {
       p1 = jsonldUtils.promises.findObjects(doc, context.Subject);
       p2 = p1.then(
         function (objects) {
-          console.log('1.1 findObjects result: %j', objects);
+          //console.log('1.1 findObjects result: %j', objects);
           objects.should.have.property('@id');
           objects.should.have.property('@type', context.Subject);
           objects.should.have.property(context.address);
@@ -76,7 +77,7 @@ describe('jsonld utils fetch objects', function () {
       p1 = jsonldUtils.promises.findObjects(doc, [context.Subject, context.SubjectIgnore]);
       p2 = p1.then(
         function (objects) {
-          console.log('1.2 findObjects result: %j', objects);
+          //console.log('1.2 findObjects result: %j', objects);
           objects.length.should.be.equal(2, 'expected two objects back');
         },
 
@@ -90,6 +91,82 @@ describe('jsonld utils fetch objects', function () {
 
       // return promise for mocha to check
       return p2;
-    }); // 1.1
+    }); // 1.2
+
+    it('1.3 findObjects pass non existent type', function () {
+      var p1, p2;
+      p1 = jsonldUtils.promises.findObjects(doc, ['bogus']);
+      p2 = p1.then(
+        function (objects) {
+          assert(!objects, util.format('no object should be found for bogus type:%j', objects));
+        },
+
+        function (reason) {
+          throw reason;
+        });
+
+      p2.catch(function (reason) {
+        throw reason;
+      });
+
+      // return promise for mocha to check
+      return p2;
+    }); // 1.3
+
+    it('1.4 findObjects pass non existent type with just a simple object', function () {
+      var p1, p2, ldoc;
+
+      ldoc = {
+        '@context': context,
+        '@id': '_:23',
+        '@type': 'Subject',
+        name: 'rich'
+      };
+
+      p1 = jsonldUtils.promises.findObjects(ldoc, ['bogus']);
+      p2 = p1.then(
+        function (objects) {
+          assert(!objects, util.format('no object should be found for bogus type:%j', objects));
+        },
+
+        function (reason) {
+          throw reason;
+        });
+
+      p2.catch(function (reason) {
+        throw reason;
+      });
+
+      // return promise for mocha to check
+      return p2;
+    }); // 1.4
+
+    it('1.5 findObjects pass valid type with just a simple object', function () {
+      var p1, p2, ldoc;
+
+      ldoc = {
+        '@context': context,
+        '@id': '_:23',
+        '@type': 'Subject',
+        name: 'rich'
+      };
+
+      p1 = jsonldUtils.promises.findObjects(ldoc, context.Subject);
+      p2 = p1.then(
+        function (objects) {
+          assert(objects, util.format('object should be found for type:%j', objects));
+        },
+
+        function (reason) {
+          throw reason;
+        });
+
+      p2.catch(function (reason) {
+        throw reason;
+      });
+
+      // return promise for mocha to check
+      return p2;
+    }); // 1.5
   }); // 1
 });
